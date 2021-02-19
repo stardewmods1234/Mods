12c9f3f1c36bfa05236f03e5da336af371ab48fd
stardewValley <stardewmods1234@gmail.com>
2021-02-19 08:43:51 -0300

    :fire: extreme delete mods

--- Ayeisha/[CP] Ayeisha/assets/images/tilesheets/README.txt
+++
@@ -1,1 +0,0 @@
-I know this is a bit of a mess. I do plan to redo the tilesheets/map patches at some point.
--- Toshinori 1.0.4/Toshinori NPC/[TMX] Toshinori Locations/sproomsts.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="sproomsts" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
- <image source="sproomsts.png" width="400" height="1264"/>
-</tileset>
